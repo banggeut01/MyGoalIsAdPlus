6a956548eae2cb4bbe4bd1fc0ca8ceb704fe5833
banggeut01 <genie121110@gmail.com>
2020-01-18 22:47:27 +0900

    200118 | 메이ㅣㅣㅣㅣㅣㅣ즈

--- baek/teat.py
+++ baek/teat.py
@@ -1,5 +1,3 @@
-# 16985.py Maaaaaaaaaze
-import copy
 import collections
 def rotation(i, j): # 반시계 방향 회전, board[i]에 추가
     tmp = [[0] * 5 for _ in range(5)]
@@ -13,8 +11,9 @@ def rotation(i, j): # 반시계 방향 회전, board[i]에 추가
     tmp[1][1], tmp[1][2] = board[i][j][1][3], board[i][j][2][3]
     tmp[1][3], tmp[2][3] = board[i][j][3][3], board[i][j][3][2]
     tmp[3][3], tmp[3][2] = board[i][j][3][1], board[i][j][2][1]
-    tmp[2][2] = board[i][j][2][2] ####
-    for a in board[i]: ####
+    # 가운데
+    tmp[2][2] = board[i][j][2][2]
+    for a in board[i]:
         if a == tmp:
             return False
     board[i].append(tmp)
@@ -27,25 +26,13 @@ def bfs(sr, sc, er, ec):
     visit[0][sr][sc] = True
     ret = 0
     tmp = len(dq)
-    # print('============================')
     while dq:
-        # print(ret)
         for _ in range(tmp):
             x, y, z = dq.popleft()
-            # print(x, y, z)
             for dx, dy, dz in (-1, 0, 0), (1, 0, 0), (0, -1, 0), (0, 1, 0), (0, 0, -1), (0, 0, 1):
                 nx, ny, nz = x + dx, y + dy, z + dz
                 if -1 < nx < 5 and -1 < ny < 5 and -1 < nz < 5 and not visit[nx][ny][nz] and miro[nx][ny][nz]:
-                    if nx == 4 and ny == er and nz == ec: 
-                        # import pprint
-                        #
-                        #
-                        # pprint.pprint(miro)
-                        # print(sr, sc, er, ec)
-                        # print(ret + 1)
-                        # print('============================')
-                        return ret + 1
-
+                    if nx == 4 and ny == er and nz == ec:
                     visit[nx][ny][nz] = True
                     dq.append((nx, ny, nz))
         ret += 1
@@ -54,7 +41,6 @@ def bfs(sr, sc, er, ec):
 
 def perm(k):
     if k == 5:
-        # print(miro)
         global MIN
         for idx in range(4):
             sr, sc = S[idx]
@@ -82,12 +68,9 @@ board = [[] for _ in range(5)]
 for i in range(5):
     board[i].append([list(map(int, input().split())) for _ in range(5)])
     for j in range(3):
-        if not rotation(i, j): ###
+        if not rotation(i, j):
             break
-# import pprint
-# pprint.pprint(board)
 visit = [False] * 5 # 판 순서
-# rot_visit = [False] * 4 # 회전 순서
 miro = []
 S = [(0, 0), (0, 4), (4, 0), (4, 4)]
 E = [(4, 4), (4, 0), (0, 4), (0, 0)]
