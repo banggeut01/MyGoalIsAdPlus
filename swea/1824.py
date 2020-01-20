d1c4c008fe02467f45b5d5c919ace0768db5278f
banggeut01 <genie121110@gmail.com>
2020-01-21 00:18:53 +0900

    dummy commit

--- swea/1824.py
+++ swea/1824.py
@@ -3,6 +3,7 @@
 # '?'에서만 백트래킹
 def hyeokming(x, y, char, d):
     while True:
+        flag = False
         s = board[x][y] # state
         if s.isdigit(): char = int(s)
         elif s == '<': d = 2
@@ -26,17 +27,74 @@ def hyeokming(x, y, char, d):
             if char == 0: char = 15
             else: char -= 1
         elif s == '?':
+            flag = True
             for dx, dy in dir:
                 nx, ny = x + dx, y + dy
+                if -1 < nx < R and -1 < ny < C:
+                    if visit[d][char][nx][ny]: return
+                    else:
+                        visit[d][char][nx][ny] = True
+                        hyeokming(x, y, char, d)
+                else:
+                    if d == 1:
+                        x = R - 1
+                        if visit[d][char][x][y]:
+                            return
+                        else:
+                            visit[d][char][x][y] = True
+                            hyeokming(x, y, char, d)
+                    elif d == 2:
+                        x = 0
+                        if visit[d][char][x][y]:
+                            return
+                        else:
+                            visit[d][char][x][y] = True
+                            hyeokming(x, y, char, d)
+                    elif d == 3:
+                        y = C - 1
+                        if visit[d][char][x][y]:
+                            return
+                        else:
+                            visit[d][char][x][y] = True
+                            hyeokming(x, y, char, d)
+                    else:
+                        y = 0
+                        if visit[d][char][x][y]:
+                            return
+                        else:
+                            visit[d][char][x][y] = True
+                            hyeokming(x, y, char, d)
 
-        if visit[d][char][x][y]: return
-        else: visit[d][char][x][y] = True
-    return
+        if not flag:
+            dx, dy = dir[d]
+            nx, ny = x + dx, y + dy
+            if -1 < nx < R and -1 < ny < C:
+                if visit[d][char][nx][ny]: return
+                else:
+                    visit[d][char][nx][ny] = True
+                    x, y = nx, ny
+            else:
+                if d == 1:
+                    x = R - 1
+                    if visit[d][char][x][y]:return
+                    else: visit[d][char][x][y] = True
+                elif d == 2:
+                    x = 0
+                    if visit[d][char][x][y]:return
+                    else: visit[d][char][x][y] = True
+                elif d == 3:
+                    y = C - 1
+                    if visit[d][char][x][y]:return
+                    else: visit[d][char][x][y] = True
+                else:
+                    y = 0
+                    if visit[d][char][x][y]:return
+                    else: visit[d][char][x][y] = True
 
 dir = [(-1, 0), (1, 0), (0, -1), (0, 1)] # 위, 아, 왼, 오
 for tc in range(1, int(input()) + 1):
     R, C = map(int, input().split())
-    board = [list(map(int, input().split())) for _ in range(R)]
+    board = [list(input()) for _ in range(R)]
     # i < 0 => i += R
     # i >= R => i -= R
     # j < 0 => j += C
