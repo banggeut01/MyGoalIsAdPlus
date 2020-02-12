bb055a7928e541e616656a8116367129200b43b3
banggeut01 <genie121110@gmail.com>
2020-02-12 13:08:09 +0900

    원자 소멸 시뮬레이션

--- swea/5648.py
+++ swea/5648.py
@@ -1,52 +1,35 @@
 # 5648.py 원자 소멸 시뮬레이션
-# 초기 보드 0
-# 이동하는데
-# 이동 전 자신의 자리에 원자가 있으면 폭발
-# 이때                새로운 자리에 표시 해준다.
-#               이동하려는 자리에 원자 있으면 폭발
-#               이때 새로운 자리에 표시해준다.
 
-def bfs():
+def simul():
+    global atom
     ret = 0
-    while len(atom) > 1:
-        idx = 0
-        board = [[0] * 4001 for _ in range(4001)]
-        while idx < len(atom):
-            num, x, y = atom[idx]
-            nx, ny = x + dir[D[num]][0], y + dir[D[num]][1]
-            if maxX < nx or maxY < ny or minX > nx or minY > ny:
-                die[num] = True
-                atom.remove((num, x, y))
-                ret += energe[num]
+    t = 0
+    while t < 4000 and len(atom) > 1:
+        tmp = dict() # 이동 후 원자들
+        for key, val in atom.items():
+            x, y = key
+            d, k = val
+            nx, ny = x + D[d][0], y + D[d][1]
+            if -2001 < nx < 2001 and -2001 < ny < 2001:
+                if tmp.get((nx, ny)):
+                    tmp[(nx, ny)].append(k)
+                else:
+                    tmp[(nx, ny)] = [d, k]
+        atom = dict()
+        for key, val in tmp.items():
+            if len(val) > 2:
+                ret += sum(val[1:])
             else:
-                if -1 < nx < 4001 and -1 < ny < 4001:
-                    if board[nx][ny]:
-                        if not die[board[nx][ny]]:
-                            die[board[nx][ny]] = True
-                            atom.remove((board[nx][ny], nx, ny))
-                            idx -= 1
-                            ret += energe[board[nx][ny]]
-                        die[num] = True
-                        ret += energe[num]
-                    else:
-                        board[nx][ny] = num
-                        idx += 1
+                atom[key] = val
+        t += 1
     return ret
 
-dir = [(-1, 0), (1, 0), (0, -1), (0, 1)] # 0상1하2좌3우
+# D = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+D = [(1, 0), (-1, 0), (0, -1), (0, 1)]
 for tc in range(1, int(input()) + 1):
-    N = int(input()) # 원자수
-    atom = []
-    die = [False] * (N + 1)
-    energe = [0] * (N + 1)
-    D = [0] * (N + 1)
-    maxX, maxY, minX, minY = 0, 0, 4000, 4000
-    for num in range(1, N + 1):
-        x, y, d, K = map(int, input().split()) # x,y : 0~4000
-        x, y = (x + 1000) * 2, (y + 1000) * 2
-        maxX, maxY, minX, minY = max(maxX, x), max(maxY, y), min(minX, x), min(minY, y)
-        atom.append((num, x, y))
-        D[num] = d
-        energe[num] = K
-    ANS = bfs()
-    print('#{} {}'.format(tc, ANS))
+    N = int(input())
+    atom = dict()
+    for _ in range(N):
+        y, x, d, k = map(int, input().split())
+        atom[(x * 2, y * 2)] = [d, k]
+    print('#{} {}'.format(tc, simul()))
