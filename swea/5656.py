16d441d9a7b84e9da8d4a30b38542a1c5b14ffd5
banggeut01 <genie121110@gmail.com>
2020-02-16 19:58:00 +0900

    벽돌 깨기

--- swea/5656.py
+++ swea/5656.py
@@ -1,5 +1,5 @@
 # 5656.py 벽돌 깨기
-import sys
+import sys, pprint
 sys.stdin = open('5656input.txt', 'r')
 import copy, collections
 def breakBrick(B): # B:보드
@@ -9,7 +9,7 @@ def breakBrick(B): # B:보드
         i = 0
         while i < H and not B[i][j]:
             i += 1
-        if i == H: continue
+        if i >= H: continue
         if B[i][j] > 1:
             dq.append((i, j, B[i][j]))
         B[i][j] = 0
@@ -17,8 +17,9 @@ def breakBrick(B): # B:보드
             x, y, val = dq.popleft()
             for dx, dy in (-1, 0), (1, 0), (0, -1), (0, 1):
                 idx = 1
+                nx, ny = x, y
                 while idx < val:
-                    nx, ny = x + dx, y + dy
+                    nx, ny = nx + dx, ny + dy
                     if -1 < nx < H and -1 < ny < W:
                         if B[nx][ny]:
                             if B[nx][ny] > 1: dq.append((nx, ny, B[nx][ny]))
@@ -31,45 +32,33 @@ def breakBrick(B): # B:보드
             for i in range(H):
                 if B[i][j]:
                     tmp.append(B[i][j])
-                B[i][j] = 0
+                    B[i][j] = 0
             for i in range(H - 1, -1, -1):
-                B[i][j] = tmp.pop()
-                if not tmp: break
+                if tmp: B[i][j] = tmp.pop()
+                else: break
     cnt = 0
-    print(B)
     for i in range(H):
         for j in range(W):
             if B[i][j]: cnt += 1
     MIN = min(MIN, cnt)
-        # for j in range(W):
-        #     i = H - 1
-        #     while B[i][j] and i > -1:
-        #         i += 1
-        #     if i > 0:
-        #         ni = i - 1
-        #         while ni > -1 and not B[ni][j]:
-        #             ni -= 1
-        #         if ni > -1 and B[ni][j]:
-        #             B[ni][j], B[i][j] = B[i][j], B[ni][j]
-        #         i = i + 1
 
+def select(k):
+    if MIN == 0: return
 
-def comb(k, s):
     if k == N:
         copied = copy.deepcopy(board)
         breakBrick(copied)
         return
 
-    for idx in range(s, W):
+    for idx in range(W):
         seq.append(idx)
-        comb(k + 1, idx + 1)
+        select(k + 1)
         seq.pop()
 
 for tc in range(1, int(input()) + 1):
     N, W, H = map(int, input().split())
     board = [list(map(int, input().split())) for _ in range(H)]
     MIN = W * H # 남은 벽돌
-    # 조합
     seq = []
-    comb(0, 0)
+    select(0)
     print('#{} {}'.format(tc, MIN))
