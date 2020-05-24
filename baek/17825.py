f362283f4a4f90b85facdf979d9ee0f527817e08
banggeut01 <genie121110@gmail.com>
2020-05-24 14:36:36 +0900

    주사위 윷놀이

--- baek/17825.py
+++ baek/17825.py
@@ -1,70 +1,57 @@
 # 17825.py 주사위 윷놀이
-
-# visit : list 0 ~ 40
-# horse : list 0~3 (말4개) 값은 현재 위치
 def back(k, total):
-    # global MAX
-    # MAX = max(MAX, total)
+    global MAX
     if k == 10:
-        global MAX
-        if MAX < total:
-            print(tmpList)
         MAX = max(MAX, total)
         return
 
+    flag = False
     for i in range(4): # i 번 말 움직이자
         if horse[i] != -1: # -1:도착
             n = li[k] # n 주사위
-            new = tmp = horse[i]
-            flag = False
+            new = origin = horse[i]
             while n > 0:
-                print(new)
-                if score[new] == 40:
-                    visit[tmp] = False
-                    horse[i] = -1
-                    tmpList.append(i)
-                    back(k + 1, total)
-                    tmpList.pop()
-                    horse[i] = tmp
-                    visit[tmp] = True
-                    break
-                else:
-                    if not flag:
-                        new = dct[new]
-                        n -= 1
-                        flag = True
-                    else:
-                        new += 1
-                        n -= 1
-            else:
-                if not visit[new]:
-                    print(score[new])
-                    visit[tmp], visit[new] = False, True
-                    horse[i] = new
-                    tmpList.append(i)
-                    back(k + 1, total + score[new])
-                    tmpList.pop()
-                    horse[i] = tmp
-                    visit[tmp], visit[new] = True, False
+                new = next[new]
+                n -= 1
+            if new in blue:
+                new = blue[new]
+            if new > 38: # 도착
+                horse[i] = -1
+                visit[origin], visit[new] = False, True
+                flag = True
+                back(k + 1, total + score[new])
+                horse[i] = origin
+                visit[origin], visit[new] = True, False
+            elif not visit[new]:
+                horse[i] = new
+                visit[origin], visit[new] = False, True
+                flag = True
+                back(k + 1, total + score[new])
+                horse[i] = origin
+                visit[origin], visit[new] = True, False
+    if not flag:
+        MAX = max(MAX, total)
 
 score = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18,
          20, 22, 24, 26, 28, 30, 32, 34, 36, 38,
-         40, 13, 16, 19, 25, 30, 35, 22, 24, 28,
-         27, 26]
-dct = {}
-for i in range(31):
-    dct[i] = i + 1
-dct[5] = 21
-dct[10] = 27
-dct[15] = 29
-dct[26] = 20
-dct[28] = 24
-dct[31] = 24
-# 출발이 40이면 ->
+         40, 10, 13, 16, 19, 25, 20, 22, 24, 25,
+         30, 28, 27, 26, 25, 25, 30, 35, 40, 0,
+         0, 0, 0, 0]
+
+next = {}
+for i in range(len(score)):
+    next[i] = i + 1
+next[19] = 38 # 38 -> 40
+next[24] = 35 # 19 -> 25
+next[28] = 35 # 24 -> 25
+next[33] = 35 # 26 -> 25
+blue = dict()
+blue[5] = 21 # 10 -> 10
+blue[10] = 26 # 20 -> 20
+blue[15] = 30 # 30 -> 30
 li = list(map(int, input().split()))
-visit = [False] * 32 # 0 - 31
-horse = [0] * 4
+visit = [False] * len(score)
+horse = [0] * 4 # 도착이면 -1
 MAX = 0
-tmpList = []
 back(0, 0)
 print(MAX)
